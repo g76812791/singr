b5d7d3cf13cd39aac8c14aefe7577d744e5ff9a4
529032234@qq.com <qq18741689356>
2016-11-29 11:29:07 +0800

    singr

---
+++ WebApplication18/MyHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace WebApplication18
+{
+    public class MyHub : Hub
+    {
+        public void Send(string message)
+        {
+            Clients.All.callBack("我是："+message);
+        }
+    }
+}
